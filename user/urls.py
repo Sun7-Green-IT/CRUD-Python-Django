7806c6c32b4f1a14fddf4886637c4b5b7acafb05
Sylvain DNS <sylvain.denyse@gmail.com>
2018-12-07 13:59:00 +0100

    fibonnaci

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListUserView
+from .views import ListUserView, ListFibonnaciView
 
 
 urlpatterns = [
-    path('user/', ListUserView.as_view(), name="user-all")
+    path('users/', ListUserView.as_view(), name="user-all"),
+    path('fibonnaci/<int:number>/',
+         ListFibonnaciView.as_view(), name="fibonnaci-all")
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
+from math import sqrt
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.response import Response
+import json
 
 
 class ListUserView(generics.ListAPIView):
@@ -9,3 +12,26 @@ class ListUserView(generics.ListAPIView):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class ListFibonnaciView(generics.ListAPIView):
+    """
+    Provides a get method handler.
+    """
+
+    def get(self, request, number):
+        print(number)
+        return(Response(json.dumps(fibo(number))))
+
+
+def fibo(number):
+    if (number == 1):
+        return [0, 1]
+    else:
+        s = fibo(number - 1)
+        s.append(s[len(s) - 1] + s[len(s) - 2])
+        return s
+
+
+def F(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
